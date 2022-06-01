5c529566fbe50a022327b6bc30302c62633ac4dc
ProMLGHacher <araik.gukasyan00@mail.ru>
2022-06-01 14:36:37 +0500

    com

--- src/main/kotlin/com/orenburg/gtrk/utils/Constants.kt
+++ src/main/kotlin/com/orenburg/gtrk/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.orenburg.gtrk.utils
 import java.time.format.DateTimeFormatter
 
 
-val serverUrl = "https://43f5-136-169-243-169.ngrok.io/"
+val serverUrl = "https://server.krea-company.keenetic.pro/"
 
 val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
 val imageUrl = serverUrl + "files/"
